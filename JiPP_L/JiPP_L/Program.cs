d9986a26883c5a32b2048b88eec4d8c758768ce2
Michal Przygoda <michalprzygoda17@gmail.com>
2025-12-11 22:23:44 +0100

    init

--- JiPP_L/JiPP_L/Program.cs
+++ JiPP_L/JiPP_L/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 25;
+float money = 12.5f;
+string text = "Example text";
+bool isLogged = true;
+char myChar = 'A';
+decimal price = 199.99m;
+
+Console.WriteLine("number: " + number);
+Console.WriteLine("money: " + money);
+Console.WriteLine("text: " + text);
+Console.WriteLine("isLogged: " + isLogged);
+Console.WriteLine("myChar: " + myChar);
+Console.WriteLine("price: " + price);
